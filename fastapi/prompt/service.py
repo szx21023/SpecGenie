6ee886442406d1b9849ed584e48d0f97e196796a
Herry Tsai <herry@cruisys.com>
2025-08-05 00:24:43 +0800

    fixed: remove useless code

--- fastapi/prompt/service.py
+++ fastapi/prompt/service.py
@@ -1,9 +1,9 @@
 from main import app
 
 from outlines import from_openai
-from openai import OpenAI
 from pydantic import BaseModel
-from typing import List
+
+from ir.schema import IR
 
 class PromptService:
     @staticmethod
@@ -30,28 +30,6 @@ class PromptService:
         """
         Test the prompt with the given ID.
         """
-
-        class Field(BaseModel):
-            name: str
-            type: str
-            primary: bool
-            foreign_key: str  # 如果不能為空，這樣寫；若允許 null 要加特別處理
-
-        class Entity(BaseModel):
-            name: str
-            fields: List[Field]
-
-        class API(BaseModel):
-            method: str
-            path: str
-            operation: str
-            entity: str
-            request_fields: List[str]
-            response_fields: List[str]
-
-        class IR(BaseModel):
-            entities: List[Entity]
-            apis: List[API]
 
         ir = await PromptService.prompt(db, prompt, IR)
         for api in ir['apis']:

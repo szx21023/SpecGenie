07b646033caed1758f38832294f7d93e416331f3
szx21023 <szx21023@gmail.com>
2025-08-25 16:09:51 +0800

    feat: add update_api api

--- fastapi/apis/service.py
+++ fastapi/apis/service.py
@@ -31,3 +31,31 @@ class ApisService:
         schema = ApisSchema(many=True)
         apis = schema.dump(apis)
         return apis
+
+    @staticmethod
+    async def update_api(db, api_id, update_json):
+        if not(api := await ApisService.get_api_by_id(db, api_id)):
+            raise Exception("Api not found")
+
+        schema = ApisSchema()
+        data = schema.load(update_json)
+
+        for key, value in data.items():
+            setattr(api, key, value)
+
+        try:
+            await db.commit()
+            await db.refresh(api)
+
+        except Exception as e:
+            await db.rollback()
+            raise e
+
+        return api
+
+    @staticmethod
+    async def get_api_by_id(db, api_id):
+        sql = select(Apis).where(Apis.id == api_id)
+        result = await db.execute(sql)
+        api = result.scalars().first()
+        return api

eae6d0492fdee28c58d6e3d271a6e69693ced377
szx21023 <szx21023@gmail.com>
2025-08-28 09:49:12 +0800

    unfinished...

--- fastapi/database.py
+++ fastapi/database.py
@@ -1,5 +1,7 @@
+from datetime import datetime
+from sqlalchemy import Integer, String, DateTime, func
 from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession
-from sqlalchemy.orm import declarative_base
+from sqlalchemy.orm import declarative_base, Mapped, mapped_column
 
 # 非同步 SQLite URI：注意是 sqlite+aiosqlite
 DATABASE_URL = "sqlite+aiosqlite:///./test.db"
@@ -12,3 +14,11 @@ Base = declarative_base()
 async def get_db() -> AsyncSession:
     async with AsyncSessionLocal() as session:
         yield session
+
+class BaseModel(Base):
+    __abstract__ = True  # 這個類不會在數據庫中創建表格
+
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
+
+    created_time: Mapped[datetime] = mapped_column(DateTime, default=func.now())
+    updated_time: Mapped[datetime] = mapped_column(DateTime, default=func.now(), onupdate=func.now())
--- fastapi/prompt/model.py
+++ fastapi/prompt/model.py
@@ -1,9 +1,9 @@
 from sqlalchemy import JSON
 from sqlalchemy.orm import Mapped, mapped_column
 
-from database import Base
+from database import Base, BaseModel
 
-class Prompts(Base):
+class Prompts(BaseModel):
     __tablename__ = "prompts"
 
     id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)

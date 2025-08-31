9d57d3b28650e53f7e49e2e9bf62ef0e24c67924
Herry Tsai <herry@cruisys.com>
2025-09-01 00:10:18 +0800

    docs: update fastapi.config.py

--- fastapi/config.py
+++ fastapi/config.py
@@ -4,4 +4,4 @@ class Config(BaseConfig):
     OPENAI_API_KEY: str = ""
     LOGGER_NAME: str|None = 'test'
 
-    DATABASE_URL : str = "sqlite+aiosqlite:///./test.db" # 非同步 SQLite URI：注意是 sqlite+aiosqlite
+    DATABASE_URL : str = "postgresql+asyncpg://postgres:postgres@localhost:5432/pgvector_demo" # 非同步 SQLite URI：注意是 sqlite+aiosqlite

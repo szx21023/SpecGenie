3ef7377c452429a87dfd750541de82bc5139593f
Herry Tsai <herry@cruisys.com>
2025-08-31 23:50:51 +0800

    refactor: add config variable: DATABASE_URL

--- fastapi/config.py
+++ fastapi/config.py
@@ -3,3 +3,5 @@ from fastapi_basic.base_config import BaseConfig
 class Config(BaseConfig):
     OPENAI_API_KEY: str = ""
     LOGGER_NAME: str|None = 'test'
+
+    DATABASE_URL : str = "sqlite+aiosqlite:///./test.db" # 非同步 SQLite URI：注意是 sqlite+aiosqlite

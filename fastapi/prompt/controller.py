9919fae289f3b1c6e2eff59e066bd500bfb97a96
szx21023 <szx21023@gmail.com>
2025-09-04 14:15:54 +0800

    remove useless code

--- fastapi/prompt/controller.py
+++ fastapi/prompt/controller.py
@@ -34,15 +34,3 @@ async def create_prompt(
     mode = schema.get('mode')
     result = await PromptService.prompt_to_model(db, user_prompt=prompt, mode=mode)
     return result
-
-@router.post("/chat")
-async def post_pomption(
-        schema = Body(example={
-            'prompt': 'Sample Prompt'
-        }),
-        db: AsyncSession = Depends(get_db)
-    ):
-        from rag_vectors.service import RagVectorService
-        prompt = schema.get('prompt')
-        result = await RagVectorService.get_rag_vector_by_prompt(db, prompt)
-        return result
